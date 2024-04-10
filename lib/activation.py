805db0768cd4b0d67d4a4b2328da55dc41039436
AustinPardosi <gabrielpardosi26@gmail.com>
2024-04-10 15:52:39 +0700

    feat: add activation function

--- lib/activation.py
+++ lib/activation.py
@@ -2,46 +2,49 @@ import numpy as np
 
 # Fungsi aktivasi reLU
 def relu(x, derivative=False):
+    x = np.array(x, dtype=float)
     if derivative:
-        # TODO : implement
-        return None
-    
+        return (x > 0).astype(float)
     return np.maximum(0, x)
 
 # Fungsi aktivasi sigmoid
 def sigmoid(x, derivative=False):
+    x = np.array(x, dtype=float)
     if derivative:
-        # TODO : implement
-        return None
-    
+        sig = 1 / (1 + np.exp(-x))
+        return sig * (1 - sig)
     return 1 / (1 + np.exp(-x))
 
 # Fungsi aktivasi linear
 def linear(x, derivative=False):
+    x = np.array(x, dtype=float)
     if derivative:
-        # TODO : implement
-        return None
-    
+        return np.ones_like(x)
     return x
 
 # Fungsi aktivasi softmax
-def softmax(x, derivative=False):
-    if derivative:
-        # TODO : implement
-        return None
-    
+def softmax(x, derivative=False, y_true=None):
+    x = np.atleast_2d(np.array(x, dtype=float))
     exp_values = np.exp(x - np.max(x, axis=1, keepdims=True))
-    return exp_values / np.sum(exp_values, axis=1, keepdims=True)
+    probabilities = exp_values / np.sum(exp_values, axis=1, keepdims=True)
+    
+    if derivative:
+        if y_true is None:
+            raise ValueError("y_true must be provided for the derivative calculation.")
+        y_true = np.atleast_2d(y_true)
+        return probabilities - y_true
+    
+    return probabilities
 
 # Mengambil nilai fungsi aktivasi berdasarkan masukan pengguna
 def get_activation_function(function_name):
-    if function_name == 'linear':
+    if function_name == "linear":
         return linear
-    elif function_name == 'relu':
+    elif function_name == "relu":
         return relu
-    elif function_name == 'sigmoid':
+    elif function_name == "sigmoid":
         return sigmoid
-    elif function_name == 'softmax':
+    elif function_name == "softmax":
         return softmax
     else:
-        raise Exception('Activation function not found.')
+        raise Exception("Activation function not found.")
